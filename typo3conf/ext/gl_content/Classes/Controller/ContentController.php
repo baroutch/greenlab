a9e4be8ba2e158badb190b30c69d9a3fc5d50c25
TABARY Olivier <olivier.tabary@wanadoo.fr>
2020-05-04 13:04:44 +0000

    instagram a finir

--- typo3conf/ext/gl_content/Classes/Controller/ContentController.php
+++ typo3conf/ext/gl_content/Classes/Controller/ContentController.php
@@ -29,4 +29,13 @@ class ContentController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControll
         return;
 
     }
+
+    public function blocInstaAction(){
+
+        $this->view->assign('photo1', $this->settings['photo1']);
+        $this->view->assign('photo2', $this->settings['photo2']);
+
+        return;
+
+    }
 }
