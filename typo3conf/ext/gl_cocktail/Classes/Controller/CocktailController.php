f27821d0b97342916eef45c6600b9d3baedc5048
TABARY Olivier <olivier.tabary@wanadoo.fr>
2020-05-07 23:12:18 +0000

    fin du plugin cocktail

--- typo3conf/ext/gl_cocktail/Classes/Controller/CocktailController.php
+++ typo3conf/ext/gl_cocktail/Classes/Controller/CocktailController.php
@@ -25,4 +25,28 @@ class CocktailController extends ActionController
         $this->view->assign('cocktail', $cocktail);
         $this->view->assign('titre', $this->settings['titre']);
     }
+
+    public function carteAction()
+    {
+        $cocktails = $this->settings['selectedCocktail-menu'];
+
+        $cocktails_tab = explode(',', $cocktails);
+
+        foreach($cocktails_tab as $k=>$row){
+            $cocktail_object[$k] = $this->cocktailRepository->findByUid($row);
+        }
+
+        foreach($cocktail_object as $k=>$row){
+            $categories = $row->getCategories()->toArray();
+
+            //print_r($categories);
+            foreach($categories as $j=>$line){
+                $output[$line->getTitle()][] = $row;
+            }/**/
+        }
+
+        //print_r($output);
+        $this->view->assign('titre-gamme', $this->settings['titre-gamme']);
+        $this->view->assign('output', $output);
+    }
 }
